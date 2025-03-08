939c145eb2b6ff8171fe72e388fced4f6042e67f
jayamour <cherryamour@naver.com>
2025-03-08 14:47:07 +0900

    LeetCode 2379

--- 2379. Minimum Recolors to Get K Consecutive Black Blocks/Solution.java
+++ 2379. Minimum Recolors to Get K Consecutive Black Blocks/Solution.java
@@ -1,7 +1,35 @@
 public class Solution {
 
 	public int minimumRecolors(String blocks, int k) {
-		return 0;
+		int l = 0;
+		int r = 0;
+
+		int ans = k;
+		int recolor = 0;
+
+		for ( r = 0; r < blocks.length(); r++ ) {
+
+			if ( blocks.charAt(r) == 'W' ) {
+				recolor++;
+			}
+
+			/**
+			 * 1234567
+			 *  L     R
+			 * WBBWWBBWBW
+			 *
+			 */
+
+			if ( r - l + 1 == k ) {
+				ans = Math.min(ans, recolor);
+				if ( blocks.charAt(l) == 'W' ) {
+					recolor--;
+				}
+				l++;
+			}
+		}
+
+		return ans;
 	}
 
 	public static void main(String[] args) {
