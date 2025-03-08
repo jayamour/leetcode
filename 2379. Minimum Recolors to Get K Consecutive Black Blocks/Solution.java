e89852ba9d10ee2b2bc00d3c8fe846c8927fbe97
jayamour <cherryamour@naver.com>
2025-03-08 13:41:32 +0900

    Apply .gitignore

---
+++ 2379. Minimum Recolors to Get K Consecutive Black Blocks/Solution.java
@@ -0,0 +1,11 @@
+public class Solution {
+
+	public int minimumRecolors(String blocks, int k) {
+		return 0;
+	}
+
+	public static void main(String[] args) {
+		Solution s = new Solution();
+		System.out.println(s.minimumRecolors("WBBWWBBWBW", 7));
+	}
+}
