c77129147dbb1a5e3807ee91f2fa0f293ed1e6bf
jayamour <cherryamour@naver.com>
2025-03-14 21:39:46 +0900

    LeetCode 35

---
+++ 35. Search Insert Position/Solution.java
@@ -0,0 +1,29 @@
+public class Solution {
+
+	public int searchInsert(int[] nums, int target) {
+
+		int left = 0;
+		int right = nums.length - 1;
+
+		while (left <= right) {
+			int mid = left + (right - left) / 2;
+
+			if (nums[mid] == target) {
+				return mid;
+			} else if (nums[mid] < target) {
+				left = mid + 1;
+			} else {
+				right = mid - 1;
+			}
+		}
+
+		return left;
+	}
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		int[] nums = {1,3,5,6};
+		int target = 7;
+		System.out.println(solution.searchInsert(nums, target));
+	}
+}
